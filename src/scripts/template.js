a31fa920f283d7d3edc917e89feda002f59ff097
Andrey Budnikov <andykov.work@gmail.com>
2018-09-10 15:03:00 +0300

    +

--- src/scripts/template.js
+++
@@ -1,6 +0,0 @@
-// var el = new SimpleBar(document.getElementById('scrl'));
-// el.getScrollElement().addEventListener('scroll', function (e) {
-//     console.log(e);
-//     console.log(1);
-// });
-
